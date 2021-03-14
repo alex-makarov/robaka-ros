8cb00adedb97f4c4090870c4f2ae63c299b868f3
Alex Makarov <makarov.alex@gmail.com>
2021-03-14 19:34:33 +0100

    Progress with move_base and AR tracking

--- src/ar_tag_tracker.py
+++ src/ar_tag_tracker.py
@@ -5,9 +5,16 @@
 # Modified for Robaka by Alex Makarov
 
 import rospy
+import actionlib
+from actionlib_msgs.msg import GoalID
 from ar_track_alvar_msgs.msg import AlvarMarkers
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import *
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from math import copysign
+import tf
+from tf import transformations as tft
+import numpy as np
+from math import cos, sin, pi
 
 class ARFollower():
     def __init__(self):
@@ -20,117 +27,85 @@ class ARFollower():
         self.rate = rospy.get_param("~rate", 10)
         r = rospy.Rate(self.rate) 
         
-        # The maximum rotation speed in radians per second
-        self.max_angular_speed = rospy.get_param("~max_angular_speed", 2.0)
-        
-        # The minimum rotation speed in radians per second
-        self.min_angular_speed = rospy.get_param("~min_angular_speed", 0.5)
-        
-        # The maximum distance a target can be from the robot for us to track
-        self.max_x = rospy.get_param("~max_x", 20.0)
-        
-        # The goal distance (in meters) to keep between the robot and the marker
-        self.goal_x = rospy.get_param("~goal_x", 0.6)
-        
-        # How far away from the goal distance (in meters) before the robot reacts
-        self.x_threshold = rospy.get_param("~x_threshold", 0.05)
-        
-        # How far away from being centered (y displacement) on the AR marker
-        # before the robot reacts (units are meters)
-        self.y_threshold = rospy.get_param("~y_threshold", 0.05)
-        
-        # How much do we weight the goal distance (x) when making a movement
-        self.x_scale = rospy.get_param("~x_scale", 0.5)
-
-        # How much do we weight y-displacement when making a movement        
-        self.y_scale = rospy.get_param("~y_scale", 1.0)
-        
-        # The max linear speed in meters per second
-        self.max_linear_speed = rospy.get_param("~max_linear_speed", 0.5)
-        
-        # The minimum linear speed in meters per second
-        self.min_linear_speed = rospy.get_param("~min_linear_speed", 0.1)
-
         # Publisher to control the robot's movement
-        self.cmd_vel_pub = rospy.Publisher('tag_follower/cmd_vel', Twist, queue_size=5)
-        
-        # Intialize the movement command
-        self.move_cmd = Twist()
+        self.move_base = actionlib.SimpleActionClient("/move_base", MoveBaseAction)
+        self.move_base.wait_for_server(rospy.Duration(60))
+        self.simple_pub=rospy.Publisher('move_base_simple/goal', PoseStamped, queue_size=1) 
+        self.cancel_pub = rospy.Publisher("/move_base/cancel", GoalID, queue_size=1)
         
         # Set flag to indicate when the AR marker is visible
         self.target_visible = False
+	self.goal_locked = False
 
-	# Set last known speed command 
-	self.last_speed = 0
-        
+        self.listener=tf.TransformListener()
+
         # Wait for the ar_pose_marker topic to become available
         rospy.loginfo("Waiting for ar_pose_marker topic...")
         rospy.wait_for_message('ar_pose_marker', AlvarMarkers)
         
         # Subscribe to the ar_pose_marker topic to get the image width and height
-        rospy.Subscriber('ar_pose_marker', AlvarMarkers, self.set_cmd_vel)
-        
-        rospy.loginfo("Marker messages detected. Starting follower...")
-        
+        rospy.Subscriber('ar_pose_marker', AlvarMarkers, self.marker_callback)
         # Begin the cmd_vel publishing loop
         while not rospy.is_shutdown():
-            # Send the Twist command to the robot
-            self.cmd_vel_pub.publish(self.move_cmd)
-            
             # Sleep for 1/self.rate seconds
             r.sleep()
 
-    def set_cmd_vel(self, msg):
+    def marker_callback(self, msg):
+#        if self.goal_locked:
+#            return
+
         # Pick off the first marker (in case there is more than one)
         try:
             marker = msg.markers[0]
             if not self.target_visible:
                 rospy.loginfo("FOLLOWER is Tracking Target!")
             self.target_visible = True
+            self.goal_locked = True
         except:
-            # If target is loar, stop the robot by slowing it incrementally
-            self.move_cmd.linear.x /= 1.5
-            self.move_cmd.angular.z /= 1.5
+            # If target is lost, cancel all goals
+            self.move_base.cancel_all_goals()
+            self.goal_locked = False
             
             if self.target_visible:
                 rospy.loginfo("FOLLOWER LOST Target!")
             self.target_visible = False
             
             return
-                
-        # Get the displacement of the marker relative to the base
-        target_offset_y = marker.pose.pose.position.y
-        
-        # Get the distance of the marker from the base
-        target_offset_x = marker.pose.pose.position.x
-        
-        # Rotate the robot only if the displacement of the target exceeds the threshold
-        if abs(target_offset_y) > self.y_threshold:
-            # Set the rotation speed proportional to the displacement of the target
-            speed = target_offset_y * self.y_scale
-            self.move_cmd.angular.z = copysign(max(self.min_angular_speed,
-                                        min(self.max_angular_speed, abs(speed))), speed)
-        else:
-            self.move_cmd.angular.z = 0.0
- 
-        # Now get the linear speed
-        if abs(target_offset_x - self.goal_x) > self.x_threshold:
-            speed = (target_offset_x - self.goal_x) * self.x_scale
-            if speed < 0:
-                speed *= 1.5
 
-	    # Simple version of slow[-er] speed ramp up
-#	    if self.last_speed < speed:
-#		speed = speed - (speed-self.last_speed)/1.2
+        # We only care for one marker for now
+        frame_id = "ar_marker_0"
 
-            self.move_cmd.linear.x = copysign(min(self.max_linear_speed, max(self.min_linear_speed, abs(speed))), speed)
-	    self.last_speed = self.move_cmd.linear.x
-        else:
-            self.move_cmd.linear.x = 0.0
+        try:
+            now=rospy.Time.now()
+            self.listener.waitForTransform('map', frame_id , now, rospy.Duration(10))
+            trans, rot=self.listener.lookupTransform('map', frame_id, now)
 
+            g = MoveBaseGoal()
+            g.target_pose.header.frame_id = 'map'
+            g.target_pose.header.stamp=rospy.Time.now()
+            x=trans[0]-0.50
+            y=trans[1]
+            z=trans[2]
+
+            rpy = tft.euler_from_quaternion(rot)
+            g.target_pose.pose.orientation = Quaternion(*tft.quaternion_from_euler(0, 0, rpy[2]-3.14159/2.0))
+            g.target_pose.pose.position.x = x
+            g.target_pose.pose.position.y = y
+            g.target_pose.pose.position.z = z
+            self.move_base.send_goal(g)
+
+            success_to_marker=self.move_base.wait_for_result()
+            if success_to_marker:
+                print "Movement to marker successful"
+            else:
+                print "Movement to marker failed"
+
+        except(tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException, tf.Exception):
+            print "TF Exception, try again"
+            
     def shutdown(self):
-        rospy.loginfo("Stopping the robot...")
-        self.cmd_vel_pub.publish(Twist())
+        rospy.loginfo("Canceling all goals")
+        self.move_base.cancel_all_goals()        
         rospy.sleep(1)     
 
 if __name__ == '__main__':
@@ -139,4 +114,3 @@ if __name__ == '__main__':
         rospy.spin()
     except rospy.ROSInterruptException:
         rospy.loginfo("AR follower node terminated.")
-
